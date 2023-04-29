1cc1b3513022783afa92455d0f745fa5ee0bc096
Anitha <anithapooth@gmail.com>
2023-04-29 18:58:06 +0200

    Co-authored-by: Ida Bjørnevik <idabj@users.noreply.github.com>

--- handwritten_digits/templatebased_classifier.py
+++ handwritten_digits/templatebased_classifier.py
@@ -1,10 +1,12 @@
 # Imports
 import numpy as np
 import pandas as pd
+import time
 import matplotlib.pyplot as plt
 from collections import Counter
 from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
+from sklearn.metrics import confusion_matrix
 import math
 from scipy.spatial import distance
 
@@ -28,7 +30,7 @@ def read_data(filename, type, size):
 x_train = read_data("handwritten_digits/train_images.bin", 'image', 60000)
 y_train = read_data("handwritten_digits/train_labels.bin", 'label', 60000)
 
-print('xtrain', x_train.shape)
+#print('xtrain', x_train.shape)
 
 x_test = read_data("handwritten_digits/test_images.bin", 'image', 10000)
 y_test = read_data("handwritten_digits/test_labels.bin", 'label', 10000)
@@ -37,39 +39,41 @@ y_test = read_data("handwritten_digits/test_labels.bin", 'label', 10000)
 def euclidean_distance(img1, img2):
     #np.linalg.norm(q-p)
     #math.dist(q, p)
-    a = img1-img2 # Since we import uint8, this will give zero for img1[i]-img2[i] < 0
-    b = np.uint8(img1<img2) * 254 + 1 # smart trick
-    return np.sum(a*b) 
+    #a = img1-img2 # Since we import uint8, this will give zero for img1[i]-img2[i] < 0
+    #b = np.uint8(img1<img2) * 254 + 1 # smart trick
+    return math.dist(img1,img2)
 
 # Calculates k nearest neighbours
 def kNN(k, x, x_train, y_train):
     distance = []
 
-    #calculate distance between each training value
+    # Calculate distance between each training value
     for ref in x_train:
         distance.append(euclidean_distance(x, ref))
-    #calculate index value of k NN
+
+
+    # Calculate index value of k NN
     k_index = np.argsort(distance)[:k]
+
     k_label = [y_train[i] for i in k_index]
-    #print(k_label)
-    #perform majority vote
+
+    # Perform majority vote
     count = Counter(k_label)
     pred = count.most_common(1)[0][0]
+
     return pred
 
 def test(k, x_test, x_train, y_train, y_test):
     prediction = []
     actual = y_test #egt ikke nødvendig
-    for i in range(20): #change
+    num_x = 10000
+    #print("actual", actual.shape)
+    for i in range(num_x): #change
         pred_value = kNN(k, x_test[i], x_train, y_train)
         prediction.append(pred_value)
-        print(f'Counter: {i}')
-        print(f'Predicted: {pred_value}, Actual: {actual[i]}\n')
-        #image = x_test[i].reshape(28,28)
-        # Display image
-        #plt.imshow(image, cmap='gray')
-        #plt.show()
-    return prediction, actual
+        #print(f'Counter: {i}')
+        #print(f'Predicted: {pred_value}, Actual: {actual[i]}\n')
+    return prediction, actual[:num_x]
 
 # Calculates error rate given expected values and predicted values
 def error_rate(expected_values, predicted_values):
@@ -84,17 +88,21 @@ def error_rate(expected_values, predicted_values):
             error += 1
     num_elems = len(expected_values)
 
+    print("Error: ", error, "\nCorrect: ", correct, "\nError rate: ", round((correct/num_elems)*100,2), "%")
+    
     return correct, error, num_elems
 
 # Calculates and displays confusion matrix
-def confusion_matrix(expected_values, predicted_values, num_classes):
-    
+def confusion_matrix(expected_values, predicted_values):
+    num_classes = 10
     # Initialize matrix
     conf_matrix = np.zeros((num_classes, num_classes))
 
     # Fill values
     for i in range(len(expected_values)):
         conf_matrix[expected_values[i]][predicted_values[i]] += 1
+
+    print("Confusion matrix\n", conf_matrix.astype(int))
 
     return conf_matrix
 
@@ -103,95 +111,73 @@ def display_image(image_array):
     plt.imshow(image_array, cmap='gray')
     plt.show()
 
-# Cluster data into 64 clusters
-""" def clustering(data):
-    kmeans = KMeans(64)
-    kmeans.fit(data)
-    print(kmeans.labels_)
-"""  """    #plt.scatter(x, y, c=kmeans.labels_) """
-
-
-""" print("x_train", x_train.shape)
-print("x_test", x_test.shape)
-print("y_train", y_train.shape)
-print("y_test", y_test.shape)
- """
-
-#prediction, actual = test(1, x_test, x_train, y_train, y_test)
-#print(confusion_matrix(prediction, actual, 10))
-
+# Sort values by class [0,9]
 def sort_values(values, labels):
-    #Sorts values by class [0-9]
-    #print(values.shape)
+    
     sorted_labels = np.argsort(labels)
     sorted_values = [values[i] for i in sorted_labels]
     sorted_values = np.array(chunks(sorted_values, 60000)).reshape(60000, 784)
     count = Counter(labels)
-    #print(count.most_common(10))
 
-    #for i in range(5):
-    #    display_image(sorted_values[5922+i].reshape(28, 28))
     return sorted_values, count
 
-#sorted_x_train = sort_values(x_train, y_train)
-
-
-#prediction, actual = test(1, x_test, x_train, y_train, y_test)
-#print(confusion_matrix(prediction, actual, 10))
-
-def sort_values(values, labels):
-    #Sorts values by class [0-9]
-    #print(values.shape)
-    sorted_labels = np.argsort(labels)
-    sorted_values = [values[i] for i in sorted_labels]
-    sorted_values = np.array(chunks(sorted_values, 60000)).reshape(60000, 784)
-    count = Counter(labels)
-    #print(count.most_common(10))
-
-    #for i in range(5):
-    #    display_image(sorted_values[5922+i].reshape(28, 28))
-    return sorted_values, count
-
-#sorted_x_train = sort_values(x_train, y_train)
-
-# This might be vary wrong, a new try is in progress
 def cluster(values, labels):
     sorted_values, count = sort_values(values, labels)
+    num_clusters = 64
 
     # Calculating indices
     starts = 0
     ends = count[0]
 
-    # 2D array for saving the centers
-    center_classes = np.zeros((10,64))
+    # Array with all cluster_centers.
+    cluster_labels = []
+    cluster_values = []
 
     # Every class
     for i in range(10):
         # 1D array for the centers in each cluster
-        center_class = np.zeros((64))
         values_one_class = sorted_values[starts:ends]
+
         starts = ends
         ends = starts + count[i]
 
-        subarray_length = int(len(values_one_class) // 64)
-        subarrays = np.array_split(values_one_class[0:subarray_length+1], 64)
+        kmeans = KMeans(n_clusters=num_clusters, n_init=10)
+        kmeans.fit(values_one_class)
         
-        # Every cluster for one clas(cluster[k]-mean)s
-        for k in range(64):
-            center = np.mean(subarrays[k])
-            center_class[k] = center
-        center_classes[i] = center_class
+        # Save values for class in array
+        
+        cluster_labels_class = [i for k in range(num_clusters)]
+        cluster_labels.append(cluster_labels_class)
 
-    print(center_classes.shape)
+        cluster_values.append(kmeans.cluster_centers_)
 
-    return center_classes
+    cluster_labels = np.array(cluster_labels).flatten()
+    cluster_values = np.array(chunks((np.array(cluster_values)).flatten(), 640)).reshape(640, 784)
 
-cluster(x_train, y_train)
+    #print("cluster_labels", cluster_labels.shape)
+    #print(cluster_labels)
+    #print("cluster_values", cluster_values.shape)
+
+    return cluster_labels, cluster_values 
 
 
+# Test for assignment part 2
+def test_clustering(k, x_test, y_test, x_train, y_train):
+    print("Starting\n")
+    t0 = time.time()
+    cluster_labels, cluster_values = cluster(x_train, y_train)
+    print("Done clustering\n")
+    print("Time clustering: ", round(time.time()-t0,2), "s")
+    prediction, actual = test(k, x_test, cluster_values, cluster_labels, y_test)
+    
+    print("Time total: ", round(time.time()-t0,2), "s")
+    return np.array(prediction), np.array(actual)
 
-# Part 2 - Clustering test
+# Test clustering
+num_k = 7
+prediction, expectation = test_clustering(num_k, x_test, y_test, x_train, y_train)
+print("prediction", prediction.shape)
+print("expectation", expectation.shape)
 
-
-#for i in range(len(prediction)):
-#    print(f'Predicted: {prediction[i]}, Actual: {actual[i]}\n')
+confusion_matrix(expectation, prediction)
+error_rate(expectation, prediction)
